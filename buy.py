f425698fe0145919c8e84d1a99a98c22f751ad54
Anton Vlasov <im.freenetwork@gmail.com>
2016-08-01 09:56:31 +0700

    Update buy.py
    1) Add "SKEY" param
    2) Fix time expiration. (In this case: Expiration 1 min. If current time more 29 sec that time expiration next minute)

--- buy.py
+++ buy.py
@@ -10,20 +10,27 @@ class Buy(Chanel):
 
     name = "buy"
 
-    def __call__(self, time, show_value):
+    def __call__(self, time, show_value, skey):
         """Method to send message to buy websocket chanel.
 
         :param msg: The websocket buy chanel message.
         """
+        
+        self.exp = None
+        
+        if (time%60) > 29:
+            self.exp = time + (60-(time%60)) + 60
+        else:
+            self.exp = time + (60-(time%60))
 
         data = dict(price=10,
                     refund_value=0,
                     act=99,
-                    exp=time + 60,
+                    exp=self.exp,
                     type="turbo",
                     direction="call",
                     value=show_value,
                     time=time)
-                    # skey="27bcbe90b8b97401a447443433531495")
+                    skey=skey)
 
         self.send_wss_request(self.name, data)
